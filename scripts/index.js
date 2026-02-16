0b9aba4be3e193db86b15c596031ced894bdc624
Molla <felipemolla2005@gmail.com>
2026-02-15 23:03:53 -0300

    feat profileEditModal

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,17 @@
+const profileEditModal = document.querySelector("#edit-popup");
+const profileEditBtn = document.querySelector(".profile__edit-button");
+const profileCloseBtn = profileEditModal.querySelector(".popup__close");
+const profileInfo = document.querySelector(".profile__info");
+const profileName = profileInfo.querySelector(".profile__title");
+const profileDescription = profileInfo.querySelector(".profile__description");
+const profileEditNameInput = document.querySelector(".popup__input_type_name");
+const profileEditDescriptionInput = document.querySelector(
+  ".popup__input_type_description",
+);
+const formEditProfile = document.querySelector("#edit-profile-form");
+const profileEditSubmitBtn =
+  formEditProfile.querySelector("#edit-profile-form");
+
 let initialCards = [
   {
     name: "Vale de Yosemite",
@@ -29,3 +43,34 @@ initialCards.forEach(function (card) {
     `O nome do cartão é: ${card.name}, e seu link é ${card.link}`,
   );
 });
+
+const openModal = (modal) => {
+  modal.classList.add("popup_is-opened");
+};
+const closeModal = (modal) => {
+  modal.classList.remove("popup_is-opened");
+};
+profileEditBtn.addEventListener("click", () => {
+  hanldeOpenEditModal(profileEditModal);
+});
+profileCloseBtn.addEventListener("click", () => {
+  closeModal(profileEditModal);
+});
+
+const fillProfileForm = () => {
+  profileEditNameInput.value = profileName.textContent;
+  profileEditDescriptionInput.value = profileDescription.textContent;
+};
+const hanldeOpenEditModal = (modal) => {
+  fillProfileForm();
+  openModal(modal);
+};
+handleProfileFormSubmit = (evt) => {
+  evt.preventDefault();
+  let nameInput = profileEditNameInput.value;
+  let jobInput = profileEditDescriptionInput.value;
+  profileName.textContent = nameInput;
+  profileDescription.textContent = jobInput;
+  closeModal(profileEditModal);
+};
+formEditProfile.addEventListener("submit", handleProfileFormSubmit);
