88336fbdcf5e410ab97c53099376e1d3e4bfe43e
Molla <felipemolla2005@gmail.com>
2026-02-17 15:12:54 -0300

    Feat card interactions

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Tripleten web_project_around_pt
 
 Este projeto traz interação e funcionalidades para a página, com o uso de JavaScript.
-Com manipulação de eventos DOM, começando pelo pop-up de edição de perfil.
+Com manipulação de eventos DOM, começando pelo pop-up de edição de perfil e agora estendendo para edição de perfil adicionando e removendo fotos.
 
 JavaScript, HTML e CSS.
--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,18 @@ const profileEditDescriptionInput = document.querySelector(
 const formEditProfile = document.querySelector("#edit-profile-form");
 const profileEditSubmitBtn =
   formEditProfile.querySelector("#edit-profile-form");
+const cardTemplate = document.querySelector("#cards-template");
+const cardsContainer = document.querySelector(".cards__list");
+const card = cardsContainer.querySelector(".card");
+const newCardModal = document.querySelector("#new-card-popup");
+const newCardForm = document.querySelector("#new-card-form");
+const newCardOpenBtn = document.querySelector(".profile__add-button");
+const newCardCloseBtn = newCardModal.querySelector(".popup__close");
+const newCardNameInput = newCardForm.querySelector(
+  ".popup__input_type_card-name",
+);
+const newCardLinkInput = newCardForm.querySelector(".popup__input_type_url");
+const newCardSubmitBtn = newCardForm.querySelector(".popup__button");
 
 let initialCards = [
   {
@@ -38,11 +50,6 @@ let initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
   },
 ];
-initialCards.forEach(function (card) {
-  return console.log(
-    `O nome do cartão é: ${card.name}, e seu link é ${card.link}`,
-  );
-});
 
 const openModal = (modal) => {
   modal.classList.add("popup_is-opened");
@@ -74,3 +81,68 @@ handleProfileFormSubmit = (evt) => {
   closeModal(profileEditModal);
 };
 formEditProfile.addEventListener("submit", handleProfileFormSubmit);
+const getCardElement = (cardName, cardLink) => {
+  const cardElement = cardTemplate.content.cloneNode(true);
+  console.log(cardElement);
+  const cardTitle = cardElement.querySelector(".card__title");
+  const cardImage = cardElement.querySelector(".card__image");
+  if (cardName == null || cardName === "") {
+    cardTitle.textContent = "Lugar sem nome";
+  } else {
+    cardTitle.textContent = cardName;
+  }
+  if (cardLink == null || cardLink === "") {
+    cardImage.src = "./images/placeholder.jpg";
+  } else {
+    cardImage.src = cardLink;
+    cardImage.alt = cardName;
+  }
+  const cardLikeBtn = cardElement.querySelector(".card__like-button");
+  const cardDeleteBtn = cardElement.querySelector(".card__delete-button");
+  cardLikeBtn.addEventListener("click", (like) => {
+    like.target.classList.toggle("card__like-button_is-active");
+  });
+  cardDeleteBtn.addEventListener("click", function (dlt) {
+    dlt.target.closest(".card").remove();
+  });
+  const cardImagePopup = cardElement.querySelector(".card__image");
+  const popupImage = document.querySelector("#image-popup");
+  const contentPopupImage = popupImage.querySelector(".popup__image");
+  const popupImageCloseBtn = popupImage.querySelector(".popup__close");
+  const contentPopupCaption = popupImage.querySelector(".popup__caption");
+  cardImagePopup.addEventListener("click", () => {
+    contentPopupImage.src = cardLink;
+    contentPopupCaption.textContent = cardName;
+    contentPopupImage.alt = cardName;
+    openModal(popupImage);
+  });
+  popupImageCloseBtn.addEventListener("click", () => {
+    closeModal(popupImage);
+  });
+  return cardElement;
+};
+const renderCard = (cardName, cardLink, cardsContainer) => {
+  const newCardElement = getCardElement(cardName, cardLink);
+  cardsContainer.prepend(newCardElement);
+};
+newCardOpenBtn.addEventListener("click", () => {
+  openModal(newCardModal);
+});
+newCardCloseBtn.addEventListener("click", () => {
+  closeModal(newCardModal);
+});
+handleCardFormSubmit = (evt) => {
+  evt.preventDefault();
+  let cardName = newCardNameInput.value;
+  let cardLink = newCardLinkInput.value;
+  renderCard(cardName, cardLink, cardsContainer);
+  closeModal(newCardModal);
+  console.log("Informações recebidas");
+};
+newCardSubmitBtn.addEventListener("click", handleCardFormSubmit);
+initialCards.forEach(function (card) {
+  renderCard(card.name, card.link, cardsContainer);
+  return console.log(
+    `O nome do cartão é: ${card.name}, e seu link é ${card.link} e foi inserido no ${cardsContainer}`,
+  );
+});
